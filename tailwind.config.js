13771ebcdbc3ec16809c77771fe933dcf5d17f5f
phamducai <giacat2910@gmail.com>
2023-02-17 14:28:33 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       textShadow: {
         md: "2px 2px 4px rgb(0 0 0 / 45%);",
       },
+      gridTemplateColumns: {
+        16: "repeat(16, minmax(0, 1fr))",
+      },
     },
   },
 
